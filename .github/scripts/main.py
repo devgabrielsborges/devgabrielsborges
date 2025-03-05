52bbdfbf9548433c828607333633cf04f424d356
devgabrielsborges <devgabrielsborges@proton.me>
2025-03-05 20:09:48 +0000

    feat: add prepare_readme and append_working_section methods to Repository class

--- .github/scripts/main.py
+++ .github/scripts/main.py
@@ -1,17 +1,11 @@
 import os
-from datetime import datetime
 from repo import Repository
 from philosophy import Philosophy
-
-count = Philosophy.get_count()
-quote = Philosophy.get_quote(count)
-
-Philosophy.update_h1(quote)
 
 oculted_repos_str = os.getenv("OCULTED_REPOS", "")
 oculted_repos = [repo.strip() for repo in oculted_repos_str.split(",")] if oculted_repos_str else []
 
-working_on_section = f"## 🚀 Working on:\n\n"
+working_on_section = "## 🚀 Working on:\n\n"
 
 repo = Repository(
     username=os.getenv("GITHUB_USERNAME"),
@@ -21,6 +15,7 @@ repo = Repository(
 )
 
 try:
+    repo.prepare_readme()
     repo.populate_active_repos()
     repo.append_working_section()
     repo.update_readme()
@@ -28,3 +23,8 @@ try:
 except Exception as e:
     print(f"Error updating README: {e}")
     exit(1)
+
+count = Philosophy.get_count()
+quote = Philosophy.get_quote(count)
+
+Philosophy.update_h1(quote)
